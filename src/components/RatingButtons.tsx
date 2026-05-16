d06189bbdea06f450966227c046f7ed62c87e919
Calle <callejohansson92@gmail.com>
2026-05-16 15:38:54 +0200

    style: vänd poängskalan – 1 först, 12 sist

--- src/components/RatingButtons.tsx
+++ src/components/RatingButtons.tsx
@@ -6,7 +6,7 @@ interface Props {
   onChange: (score: number) => void;
 }
 
-const SCORES = [12, 10, 8, 7, 6, 5, 4, 3, 2, 1];
+const SCORES = [1, 2, 3, 4, 5, 6, 7, 8, 10, 12];
 
 export function RatingButtons({ value, onChange }: Props) {
   return (
